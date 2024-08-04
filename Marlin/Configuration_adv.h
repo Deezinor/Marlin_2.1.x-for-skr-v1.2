7ab6c3588f2bdbc315cbda59b9e94c26f7755890
Jake Boulton <114814282+Deezinor@users.noreply.github.com>
2024-08-04 14:45:38 +0100

    Changed Bed_leveling type
    Bilinear to UBL

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3103,12 +3103,12 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD //JMB
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
+  #define X_HYBRID_THRESHOLD     150  // [mm/s] //JMB
+  #define X2_HYBRID_THRESHOLD    150            //JMB
+  #define Y_HYBRID_THRESHOLD     150            //JMB
+  #define Y2_HYBRID_THRESHOLD    150            //JMB
   #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
